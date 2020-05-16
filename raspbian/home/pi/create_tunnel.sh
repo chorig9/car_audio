3cf539751ba5f27ad708cc36d6658ee490c8c601
Igor Chorążewicz <chorig9@gmail.com>
2020-05-16 11:19:48 +0100

    Initial

---
+++ raspbian/home/pi/create_tunnel.sh
@@ -0,0 +1,6 @@
+if ! ssh -i /home/pi/aws_micro_ubuntu.pem ubuntu@ec2-18-218-34-216.us-east-2.compute.amazonaws.com -t 'timeout 5 ssh -p 10022 pi@localhost exit'; then
+	ssh -i /home/pi/aws_micro_ubuntu.pem ubuntu@ec2-18-218-34-216.us-east-2.compute.amazonaws.com -t 'sudo kill -9 $(sudo lsof -t -i:10022) && sudo systemctl restart ssh' || true
+	sleep 5
+	ssh -i /home/pi/aws_micro_ubuntu.pem ubuntu@ec2-18-218-34-216.us-east-2.compute.amazonaws.com -R 10022:localhost:22 -o ServerAliveInterval=60 -f -N
+fi
+
