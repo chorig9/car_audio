c0baf84741d5802de20fe3819c8c8032787a2709
Igor Chorazewicz <chorig9@gmail.com>
2020-07-25 19:26:31 +0200

    Exit on port fowarding error

--- raspbian/setup_ssh_tunnel.sh
+++ raspbian/setup_ssh_tunnel.sh
@@ -1,2 +1,2 @@
-ssh -i /home/pi/aws_micro_ubuntu.pem ubuntu@ec2-18-218-34-216.us-east-2.compute.amazonaws.com -R 10022:localhost:22 -o ServerAliveInterval=60 -NT
+ssh -i /home/pi/aws_micro_ubuntu.pem ubuntu@ec2-18-218-34-216.us-east-2.compute.amazonaws.com -R 10022:localhost:22 -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -NT
 
