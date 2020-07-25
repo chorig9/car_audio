f81ca7575e91b875081b244a7fbbcdd7c22f0fef
Igor Chorazewicz <chorig9@gmail.com>
2020-07-25 22:06:03 +0200

    add last.target

--- raspbian/install.sh
+++ raspbian/install.sh
@@ -1,11 +1,21 @@
 cp 70-usb-modeswitch.rules /etc/udev/rules.d/
 
+cp last.target /etc/systemd/system/last.target
+mkdir /etc/systemd/system/last.target.wants
+
 cp network-wait-online.service /lib/systemd/system/
 cp client_init.service /etc/systemd/system/
 cp ssh_tunnel.service /etc/systemd/system/
 cp http_tunnel.service /etc/systemd/system/
 
+ln -s /etc/systemd/system/ssh_runnel.service /etc/systemd/system/last.target.wants/ssh_tunnel.service
+ln -s /etc/systemd/system/http_runnel.service /etc/systemd/system/last.target.wants/http_tunnel.service
+
 systemctl daemon-reload
+
+systemctl set-default last.target
+systemctl isolate last.target
+
 systemctl enable network-wait-online.service
 systemctl enable client_init.service
 systemctl enable ssh_tunnel.service
