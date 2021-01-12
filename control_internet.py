19cb3912a2bc5391c563c24a04f733bedeb23da3
test <test>
2021-01-12 18:54:32 +0000

    fix scripts

--- control_internet.py
+++ control_internet.py
@@ -3,16 +3,20 @@ import time
 import subprocess
 
 def loading_event(channel):
+	time.sleep(2)
+
 	if GPIO.input(channel):
 		print("not loading")
-		subprocess.run(["ifconfig", "eth1", "down"])
+		subprocess.run(["ifconfig", "eth0", "down"])
 	else:
 		print("loading")
-		subprocess.run(["ifconfig", "eth1", "up"])
+		subprocess.run(["ifconfig", "eth0", "up"])
+		time.sleep(5)
+		subprocess.run(["systemctl", "restart", "raspotify.service"])
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(26, GPIO.BOTH, callback=loading_event, bouncetime=200)
+GPIO.add_event_detect(26, GPIO.BOTH, callback=loading_event)
 
 while True:
 	time.sleep(10)
