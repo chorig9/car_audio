e497a3721cd56e0b515d098aa6addeeee1590cd2
Igor Chorążewicz <chorig9@gmail.com>
2021-10-17 21:25:24 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
-print("ECHO")
+import time
+import subprocess
+
+from spotify_controller.controller import *
+
+spotify = None
+
+while spotify is None:
+	time.sleep(10)
+	try:
+		spotify = SpotifyController()
+	except Error as e:
+		print(e)
+
+spotify.play()
+
